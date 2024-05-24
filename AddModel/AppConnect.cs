94bc54760d54b6705f4509166c9afd82fff38909
ElinaSmirnova1607 <elinasmir16@mail.ru>
2024-05-24 14:16:06 +1000

    Добавьте файлы проекта.

---
+++ AddModel/AppConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kolesnichenko_11.AddModel
+{
+    internal class AppConnect
+    {
+        public static Task_11Entities model1db;
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using kolesnichenko_11.AddModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kolesnichenko_11
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AppConnect.model1db = new Task_11Entities(); //подключение базы данных
+            AppFrame.FrameMain = FrmMain; //подключение фрейма с началом работы программы
+
+            Client.ItemsSource = AddModel.Task_11Entities.GetContext().Client.ToList();
+        }
+    }
+}
